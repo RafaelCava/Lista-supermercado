2085583d13b194fb81eb583119ceeb6ca31eaf03
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2021-07-03 00:30:43 -0300

    -Melhorando o JS -Inserindo input que Remove itens específicos -finalizando Projeto

--- JS/supermercado.js
+++ JS/supermercado.js
@@ -1,6 +1,6 @@
 let items = [];
 
-document.querySelector("input[type=submit").addEventListener('click',()=>{
+const addTexto = () =>{
     let nomeProduto = document.getElementById("nome_produto");
     let precoProduto = document.getElementById("valor_produto");
     
@@ -12,7 +12,6 @@ document.querySelector("input[type=submit").addEventListener('click',()=>{
     let listaProdutos = document.querySelector(".lista-produtos")
     let soma = 0;
     
-    // criar uma function utilizando o map para ativar so quando eu mandar
     listaProdutos.innerHTML = "";
     items.map((val)=>{
         soma += parseFloat(val.valor);
@@ -32,20 +31,42 @@ document.querySelector("input[type=submit").addEventListener('click',()=>{
     
     let somaProduto = document.querySelector(".soma-produto h1");
     somaProduto.innerHTML = "R$ "+soma;
-    console.log(items)
-})
+}
 
-document.querySelector("button[name=limpar]").addEventListener('click',()=>{
+const delList = () =>{
     items = [];
 
     document.querySelector(".lista-produtos").innerHTML = ""
     document.querySelector(".soma-produto h1").innerHTML = "R$ 0"
-})
+}
 
+const delItem = () =>{
+    let inputValue = document.querySelector(".delete input[type=text]");
+    let index = items.findIndex(x=> x.nome === inputValue.value);
+    let soma = 0;
+    if(index > -1){
+        items.splice(index, 1);
+        let listaProdutos = document.querySelector(".lista-produtos");
+        listaProdutos.innerHTML = "";
+        items.map((val)=>{
+            soma += parseFloat(val.valor);
+            listaProdutos.innerHTML += `
+            <div class="lista-produto-single">
+            <h3>${val.nome}</h3>
+            <h3 class="price-produto"><span>R$${val.valor}<span></h3>
+            </div>
+            
+            `
+        })
+        soma = soma.toFixed(2)
+        inputValue.value = "";
+        let somaProduto = document.querySelector(".soma-produto h1");
+        somaProduto.innerHTML =`Total : R$`+soma;
+    }else{
+        alert(`Esse item não está na lista`)
+    }
+}
 
-document.querySelector("button[name=delete-btn]").addEventListener('click',()=>{
-    let encontrado = document.querySelector(".delete input");
-    encontrado = encontrado.value;
-    let index = items.filter(x=> x.nome = encontrado ? x : null);
-    alert(index.nome)
-})
+document.querySelector("input[type=submit").addEventListener('click',addTexto);
+document.querySelector("button[name=limpar]").addEventListener('click',delList);
+document.querySelector("button[name=delete-btn]").addEventListener('click',delItem);
